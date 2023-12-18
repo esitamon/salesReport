389a7380cdb081fd5cfe88f6ead9a23ed1067e65
E. SITAMON <sitamonemmanuel@gmail.com>
2023-12-17 19:20:07 -0600

    adding data loading functionality

--- FileLoader.py
+++ FileLoader.py
@@ -1,26 +1,48 @@
 import csv
+from Team import Team
 
 class FileLoader:
     def __init__(self,team_file,product_file,sale_file):
         self.team_file = team_file
         self.product_file = product_file
         self.sale_file = sale_file
-        self.team = []
-        self.product = []
+        self.teams = []
+        self.products = []
         self.sales = []
         self.print(".......Initializing report data.........")
   
 
     def __load_team(self):
-        pass
+        try:
+            with open(self.team_file, newline='') as file:
+                csv_reader =  csv.DictReader(file)
+                for row in csv_reader:
+                    team = Team(self.__remove_bom(row['TeamID']),row['Name'])
+                    self.teams.append(team)
+                    self.print(f"Team: {team.teamId} - {team.teamName}")
+                for person in self.teams:
+                    print(f"Team: {person.teamId} - {person.teamName}")
+                return self.teams
+        except FileNotFoundError:
+            print(f"Error: File '{self.team_file}' not found.")
+        except Exception as e:
+            print(f"Error: {e.with_traceback}")
+
+        
+
+        
     def __load_product(self):
         pass
     def __load_sale(self):
         pass
 
+    def __remove_bom(self,string):
+        # this method removes the BOM character from the beginning of the string
+        return string.replace("\ufeff", "")
+
     def load_all(self):
-        self.team = self.__load_team()
-        self.product= self.__load_product()
+        self.teams = self.__load_team()
+        self.products= self.__load_product()
         self.sales = self.__load_sale()
         self.print(f"........All data loaded........")
 
@@ -28,12 +50,12 @@ class FileLoader:
         print(f"{msg}")
 
     def get_products(self):
-        return self.product
+        return self.products
     
     def get_sales(self):
         return self.sales
     
     def get_team(self):
-        return self.team
+        return self.teams
     
 
