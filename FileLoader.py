c4c3ed37f9d19d34c5eb19b1274ac696cb9045b9
E. SITAMON <sitamonemmanuel@gmail.com>
2023-12-17 18:14:15 -0600

    Add funcitonality to get file path

--- FileLoader.py
+++ FileLoader.py
@@ -2,13 +2,13 @@ import csv
 
 class FileLoader:
     def __init__(self,team_file,product_file,sale_file):
-        print(f"Initializing report data")
         self.team_file = team_file
         self.product_file = product_file
         self.sale_file = sale_file
         self.team = []
         self.product = []
         self.sales = []
+        self.print(".......Initializing report data.........")
   
 
     def __load_team(self):
@@ -22,7 +22,10 @@ class FileLoader:
         self.team = self.__load_team()
         self.product= self.__load_product()
         self.sales = self.__load_sale()
-        self.print(f"All data loaded")
+        self.print(f"........All data loaded........")
+
+    def print(self,msg):
+        print(f"{msg}")
 
     def get_products(self):
         return self.product
--- report.py
+++ report.py
@@ -1,24 +1,49 @@
 import sys
+import os
 import argparse
 from FileLoader import FileLoader
 from ReportProcessor import ReportProcessor
 
 
-
-#setting main as the starting function
-if __name__ =='__main__':
-    print("--------------------Starting  the program------------------")
-    #Parsing arguments
+def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", help="File to load")
-    #load all files
+    parser.usage = "python report.py --team team.csv --sales sales.csv --products products.csv"
+    parser.add_argument("--team", help="positional argument for loading team.csv", required=True)
+    parser.add_argument("--sales", help="positional argument for loading sales.csv", required=True)
+    parser.add_argument("--products", help="positional argument for loading products.csv", required=True)
+    args = parser.parse_args()
+    return args
 
-    file_loader = FileLoader()
+def main():
+
+    print("--------------------Starting the program------------------")
+
+    # Parsing arguments
+    args = parse_arguments()
+
+    # Files path
+    cwd_path  = os.path.dirname(os.path.abspath(__file__))
+    team_file = os.path.join(cwd_path,"resources\\"+args.team)
+    sales_file = os.path.join(cwd_path,"resources\\"+args.sales)
+    product_file = os.path.join(cwd_path,"resources\\"+args.products)
+
+    print(f"team file: {team_file}")
+    print(f"sales file: {sales_file}")
+    print(f"product file: {product_file}")
+    print(f"CWD Path: {cwd_path}")
+
+    # Load all files
+
+    file_loader = FileLoader(team_file,product_file,sales_file)
     file_loader.load_all()
 
-    #run the processor to calculate report values
-    processor = ReportProcessor(file_loader.get_sales,file_loader.get_team,file_loader.get_products)
-    processor.write_sales_by_product_report()
-    processor.write_sales_by_team_report()
+    # Run the processor to calculate report values
+    # processor = ReportProcessor(file_loader.get_sales, file_loader.get_team, file_loader.get_products)
+    # processor.write_sales_by_product_report()
+    # processor.write_sales_by_team_report()
+    #setting main as the starting function
+
+if __name__ =='__main__':
+    main()
 
 
